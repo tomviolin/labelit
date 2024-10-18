e568fbc28989ad65ff2e229b93f13a6dbf024688
Tom Hansen <tomh@uwm.edu>
2024-10-18 16:25:23 -0500

    dragging works partially

--- entertestdata2.py
+++ entertestdata2.py
@@ -298,6 +298,10 @@ while True:
     rx1=None
     ry1=None
     dragging_mode = False
+    dragging_origin_x = None
+    dragging_origin_y = None
+    dragging_ann = None
+
     pause_mode = True
     one_shot = False
     pos_frames = 0
@@ -320,6 +324,8 @@ while True:
 
 
     while ok:
+        save_flag = False
+        flash = (int(time.time()*3)%2) > 0
         frames_to_read = 50
         frames_to_skip = 5
         if hires_mode:
@@ -409,25 +415,42 @@ while True:
                 #         ex[0]+ex[2]
                 #
                 if mx/size_ratio_x >= ex[0] and mx/size_ratio_x <= ex[0]+ex[2] and my/size_ratio_y >= ex[1] and my/size_ratio_y <= ex[1]+ex[3]:
-                    cv2.rectangle(displayframe,ex,(255,255,0),1)
+                    cv2.rectangle(displayframe,ex,(255,127,127),1)
+                    if dragging_mode:
+                        cv2.rectangle(displayframe,ex,(255,255,127),1)
                     hovering_over = ai
                     # determine which diagonal-delimited quadrant the mouse is in
-                    if mx/size_ratio_x < ex[0]+ex[2]//2:
+                    rect_center_x = ex[0]+ex[2]//2
+                    rect_center_y = ex[1]+ex[3]//2
+                    min_width = min(ex[2],ex[3])//4
+                    dist_from_center_x = mx/size_ratio_x - rect_center_x
+                    dist_from_center_y = my/size_ratio_y - rect_center_y
+                    dist_from_center = np.sqrt(dist_from_center_x**2 + dist_from_center_y**2)
+                    if dist_from_center < min_width:
+                        #cv2.putText(displayframe,"C",(ex[0]+ex[2]//2,ex[1]+ex[3]//2),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                        cv2.drawMarker(displayframe,(ex[0]+ex[2]//2,ex[1]+ex[3]//2),(255,255,255),cv2.MARKER_CROSS,10,2)
+                        hovering_over_quad = "C"
+                    elif mx/size_ratio_x < ex[0]+ex[2]//2:
                         if my/size_ratio_y < ex[1]+ex[3]//2:
-                            cv2.putText(displayframe,"NW",(ex[0],ex[1]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            #cv2.putText(displayframe,"NW",(ex[0],ex[1]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            cv2.drawMarker(displayframe,(ex[0],ex[1]),(255,255,255),cv2.MARKER_CROSS,10,2)
                             hovering_over_quad = "NW"
                         else:
-                            cv2.putText(displayframe,"SW",(ex[0],ex[1]+ex[3]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            #cv2.putText(displayframe,"SW",(ex[0],ex[1]+ex[3]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            cv2.drawMarker(displayframe,(ex[0],ex[1]+ex[3]-1),(255,255,255),cv2.MARKER_CROSS,10,2)
                             hovering_over_quad = "SW"
                     else:
                         if my/size_ratio_y < ex[1]+ex[3]//2:
-                            cv2.putText(displayframe,"NE",(ex[0]+ex[2],ex[1]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            #cv2.putText(displayframe,"NE",(ex[0]+ex[2],ex[1]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            cv2.drawMarker(displayframe,(ex[0]+ex[2]-1,ex[1]),(255,255,255),cv2.MARKER_CROSS,10,2)
                             hovering_over_quad = "NE"
                         else:
-                            cv2.putText(displayframe,"SE",(ex[0]+ex[2],ex[1]+ex[3]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            #cv2.putText(displayframe,"SE",(ex[0]+ex[2],ex[1]+ex[3]),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
+                            cv2.drawMarker(displayframe,(ex[0]+ex[2]-1,ex[1]+ex[3]-1),(255,255,255),cv2.MARKER_CROSS,10,2)
                             hovering_over_quad = "SE"
                     if deleting_mode:
-                        cv2.putText(displayframe,"deleting",(ex[0],ex[1]-10),cv2.FONT_HERSHEY_DUPLEX,1,(  0,191,255),2)
+                        if flash:
+                            cv2.putText(displayframe,"deleting",(ex[0],ex[1]-10),cv2.FONT_HERSHEY_DUPLEX,1,(  0,127,255),2)
                     else:
                         cv2.putText(displayframe,f"hov: {pos_frames}:{hovering_over}",(ex[0],ex[1]-10),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2)
                 '''
@@ -453,7 +476,6 @@ while True:
         # print(f"frame redrawn: pos_frames={pos_frames}")
         # check for keyboard events
         k = cv2.waitKeyEx(1) # & 0xFF
-        save_flag = False
         if (k != 255 and k != -1):
             print(f"k={k}")
         if k == ord('q'):
@@ -558,7 +580,10 @@ while True:
                     pause_mode = True
 
         elif (k == 81 or k == 65361 or k == ord('a')) and hovering_over is not None: # left arrow
-            if hovering_over_quad == "NW":
+            if hovering_over_quad == "C":
+                annotations[pos_frames][hovering_over][0] -= 1
+                save_flag = True
+            elif hovering_over_quad == "NW":
                 annotations[pos_frames][hovering_over][0] -= 1
                 annotations[pos_frames][hovering_over][2] += 1
                 save_flag = True
@@ -573,7 +598,10 @@ while True:
                 annotations[pos_frames][hovering_over][2] -= 1
                 save_flag = True
         elif (k == 83 or k == 65363 or k == ord('d')) and hovering_over is not None: # right arrow
-            if hovering_over_quad == "NW":
+            if hovering_over_quad == "C":
+                annotations[pos_frames][hovering_over][0] += 1
+                save_flag = True
+            elif hovering_over_quad == "NW":
                 annotations[pos_frames][hovering_over][0] += 1
                 annotations[pos_frames][hovering_over][2] -= 1
                 save_flag = True
@@ -587,8 +615,11 @@ while True:
             elif hovering_over_quad == "SE":
                 annotations[pos_frames][hovering_over][2] += 1
                 save_flag = True
-        elif (k == 84 or k == 65364 or k == ord('w')) and hovering_over is not None: # down arrow
-            if hovering_over_quad == "NW":
+        elif (k == 84 or k == 65364 or k == ord('s')) and hovering_over is not None: # down arrow
+            if hovering_over_quad == "C":
+                annotations[pos_frames][hovering_over][1] += 1
+                save_flag = True
+            elif hovering_over_quad == "NW":
                 annotations[pos_frames][hovering_over][1] += 1
                 annotations[pos_frames][hovering_over][3] -= 1
                 save_flag = True
@@ -602,7 +633,10 @@ while True:
             elif hovering_over_quad == "SE":
                 annotations[pos_frames][hovering_over][3] += 1
                 save_flag = True
-        elif (k == 82 or k == 65362 or k == ord('s')) and hovering_over is not None: # up arrow
+        elif (k == 82 or k == 65362 or k == ord('w')) and hovering_over is not None: # up arrow
+            if hovering_over_quad == "C":
+                annotations[pos_frames][hovering_over][1] -= 1
+                save_flag = True
             if hovering_over_quad == "NW":
                 annotations[pos_frames][hovering_over][1] -= 1
                 annotations[pos_frames][hovering_over][3] += 1
@@ -674,109 +708,61 @@ while True:
                 zoom_rect = False
                 mdblclk = True
             elif event == cv2.EVENT_MOUSEMOVE:
-                # scan existing annotations to see if we are hovering over one
-                '''if pos_frames in annotations:
-                    for an in annotations[pos_frames]:
-                        if zoom_rect:
-                            ex0 = an[0]-zx0
-                            ex1 = an[0]+an[2]-zx0
-                            ey0 = an[1]-zy0
-                            ey1 = an[1]+an[3]-zy0
-                        else:
-                            ex0 = an[0]
-                            ex1 = an[0]+an[2]
-                            ey0 = an[1]
-                            ey1 = an[1]+an[3]
-                        if mx >= ex0 and mx <= ex1 and my >= ey0 and my <= ey1:
-                            if deleting_mode:
-                                cv2.rectangle(displayframe,(ex0,ey0),(ex1,ey1),(0,255,0),1) #(0,255,255),1)
-                            else:
-                                cv2.rectangle(displayframe,(ex0,ey0),(ex1,ey1),(0,255,0),1) # (0,0,255),1)
-                            break
-                            rect_transp_fill(displayframe,(ex0,ey0,ex1,ey1),(0,255,0),0.3,1) #(0,0,255),0.3,1)
-                            '''
+                if dragging_mode:
+                    if dragging_ann is not None:
+                        if hovering_over_quad == "C":
+                            annotations[pos_frames][dragging_ann][0] += mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][1] += my-dragging_origin_y
+                        elif hovering_over_quad == "NW":
+                            annotations[pos_frames][dragging_ann][0] += mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][1] += my-dragging_origin_y
+                            annotations[pos_frames][dragging_ann][2] -= mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][3] -= my-dragging_origin_y
+                        elif hovering_over_quad == "NE":
+                            annotations[pos_frames][dragging_ann][2] += mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][1] += my-dragging_origin_y
+                            annotations[pos_frames][dragging_ann][3] -= my-dragging_origin_y
+                        elif hovering_over_quad == "SW":
+                            annotations[pos_frames][dragging_ann][3] += my-dragging_origin_y
+                            annotations[pos_frames][dragging_ann][0] += mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][2] -= mx-dragging_origin_x
+                        elif hovering_over_quad == "SE":
+                            annotations[pos_frames][dragging_ann][2] += mx-dragging_origin_x
+                            annotations[pos_frames][dragging_ann][3] += my-dragging_origin_y
+                        annotations[pos_frames][dragging_ann][0] = np.clip(annotations[pos_frames][dragging_ann][0],0,frames[0].shape[1]-5)
+                        annotations[pos_frames][dragging_ann][1] = np.clip(annotations[pos_frames][dragging_ann][1],0,frames[0].shape[0]-5)
+                        annotations[pos_frames][dragging_ann][2] = np.clip(annotations[pos_frames][dragging_ann][2],10,frames[0].shape[1]-5)
+                        annotations[pos_frames][dragging_ann][3] = np.clip(annotations[pos_frames][dragging_ann][3],10,frames[0].shape[0]-5)
+
+                        dragging_origin_x = mx
+                        dragging_origin_y = my
+                        save_flag = True
             elif event == cv2.EVENT_LBUTTONDOWN:
                 # scan existing annotations to see if we clicked inside one
-                if pos_frames in annotations:
-                    for ai in range(len(annotations[pos_frames])):
-                        an = annotations[pos_frames][ai]
-                        if zoom_rect:
-                            cv2.rectangle(displayframe,(int(an[0]-zx0),int(an[1]-zy0)),(int(an[0]+an[2]-zx0),int(an[1]+an[3]-zy0)),(0,255,255),1)
-                            ex0 = an[0]-zx0
-                            ex1 = an[0]+an[2]-zx0
-                            ey0 = an[1]-zy0
-                            ey1 = an[1]+an[3]-zy0
-
-                        else:
-                            # cv2.rectangle(displayframe,(an[0],an[1]),(an[0]+an[2],an[1]+an[3]),(0,0,255),1)
-                            cv2.rectangle(displayframe,an,(0,255,255),1)
-                            ex0 = an[0]
-                            ex1 = an[0]+an[2]
-                            ey0 = an[1]
-                            ey1 = an[1]+an[3]
-                        if mx >= ex0 and mx <= ex1 and my >= ey0 and my <= ey1:
-                            #editing_rect = True
-                            pass
-                            #break
-
-                #cv2.rectangle(displayframe,(1,1),(displayframe.shape[1]-2,displayframe.shape[0]-2),(0,0,255),2)
-                roi = list(cv2.selectROI('frame', displayframe, True,False))
-                if roi is None:
-                    continue
-                print(roi)
-                if tuple(roi) != (0,0,0,0):
-                    roi = [ int(roi[0]*size_ratio_x), int(roi[1]*size_ratio_y), int(roi[2]*size_ratio_x), int(roi[3]*size_ratio_y)]
-                    print(roi)
-                    if zoom_rect:
-                        roi = [roi[0]+zx0, roi[1]+zy0,roi[2],roi[3]]
-                    if pos_frames in annotations:
-                        annotations[pos_frames].append(roi)
-                    else:
-                        annotations[pos_frames] = [ roi ]
-
-                    os.makedirs(os.path.dirname(labelsfi), exist_ok=True)
-                    ww = frames.shape[2] #displayframe.shape[1]
-                    wh = frames.shape[1] #displayframe.shape[0]
-                    # convert to yolo format
-                    yoloroi = rect2yolo(roi,ww,wh)
-                    # write to file - we know it's a new annotation so we can append
-                    open(labelsfi,"a").write(f"{pos_frames} 0 {yoloroi[0]} {yoloroi[1]} {yoloroi[2]} {yoloroi[3]}\n");
-                    # save pos_frames
-                    open("posframes.txt","w").write(f"{pos_frames}")
+                if hovering_over is not None and hovering_over >= 0 and hovering_over < len(annotations[pos_frames]):
+                    dragging_mode = True
+                    dragging_ann = hovering_over
+                    dragging_origin_x = mx
+                    dragging_origin_y = my
                 else:
-                    # if selecting an ROI was cancelled, we are done
-                    '''
-                    if editing_rect:
-                        if pos_frames in annotations:
-                            for i in range(len(annotations[pos_frames])):
-                                an = annotations[pos_frames][i]
-                                an = a
-                                if zoom_rect:
-                                    ex0 = an[0]-zx0
-                                    ex1 = an[0]+an[2]-zx0
-                                    ey0 = an[1]-zy0
-                                    ey1 = an[1]+an[3]-zy0
-                                else:
-                                    ex0 = an[0]
-                                    ex1 = an[0]+an[2]
-                                    ey0 = an[1]
-                                    ey1 = an[1]+an[3]
-                                if mx >= ex0 and mx <= ex1 and my >= ey0 and my <= ey1:
-                                    del annotations[pos_frames][i]
-                                    # write to file
-                                    # must rewrite the entire file
-                                    open(labelsfi,"w").write("")
-                                    for frame in annotations:
-                                        for an in annotations[pos_frames]:
-                                            yoloroi = rect2yolo(an,ww,wh)
-                                            open(labelsfi,"a").write(f"{frame} 0 {yoloroi[0]} {yoloroi[1]} {yoloroi[2]} {yoloroi[3]}\n")
-                                    break
+                    dragging_mode = False
+                    dragging_ann = None
+                    newan = [mx-5,my-5,10,10]
+                    if zoom_rect:
+                        newan[0] += zx0
+                        newan[1] += zy0
+                    if pos_frames in annotations:
+                        annotations[pos_frames].append(newan)
+                    else:
+                        annotations[pos_frames] = [ newan ]
+                    save_flag = True
 
 
-                    editing_rect = False
-                    '''
-                cv2.setMouseCallback('frame',mouse_event)
+            elif event == cv2.EVENT_LBUTTONUP:
+                dragging_mode = False
+                dragging_ann = None
 
+        heywood = 0
         """
         if mevent is not None:
             if mevent['e'] == cv2.EVENT_MOUSEMOVE:
